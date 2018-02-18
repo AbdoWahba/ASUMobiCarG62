68c80c5231513cefdeb8b08e1e9fdb150ec01ca2
Ahmed Nasr <Ahmed.nasr9677@gmail.com>
2018-02-19 00:50:49 +0200

    First driver

---
+++ UART.c
@@ -0,0 +1,83 @@
+/*
+ * UART.c
+ *
+ * Created: 2/19/2018 12:26:56 AM
+ *  Author: Ahmed
+ */ 
+ #include "UART.h"
+
+ 
+ /************************ Functions Definitions *******************************/
+
+
+ void UART_init(){
+	 /* The function initializes the UART module 
+	 * Enable Transimiting and Receiving
+	 * No Parity check - 8 bit data in the frame
+	 * 1 stop bit
+	 * set BAUD Rate
+	 */
+	 UCSRB = (1 << TXEN) | (1 << RXEN);
+	 UCSRC = (1 << URSEL) | (1 << UCSZ0) | (1 << UCSZ1);
+	 UBRRL = UBRR_VALUE;
+	 UBRRH = UBRR_VALUE >> 8;
+ }
+
+
+
+ void UART_sendByte(uint8_t data) {
+	 /* waiting till UDR register is empty
+	 * send data
+	 * waiting till TXC flag is set
+	 * clear flag
+	 */
+	 while (!(UCSRA & (1 << UDRE)));
+	 UDR = data;
+	 while (!(UCSRA & (1 << TXC)));
+	 UCSRA |= (1 << TXC);
+ }
+
+ uint8_t UART_receiveByte() {
+	 /* waiting till RXC flag is set
+	 * clear the flag 
+	 * return data
+	 *
+	 */
+	 while  (!(UCSRA & (1 << RXC)));
+	 UCSRA |= (1 << RXC);
+	 return UDR;
+
+ }
+
+ void UART_sendString(const uint8_t *str) {
+	/*
+
+
+	*/
+	 while (*str != '\0') {
+		 UART_sendByte(*str);
+		 str++;
+	 }
+
+ }
+
+
+ void UART_receiveString(uint8_t *str) {
+
+	 uint8_t i = 0;
+	 str[i] = UART_receiveByte();
+
+	 while(i != '#') {
+		 str[i++] = UART_receiveByte();
+
+	 }
+	 str[i] = '\0';
+
+ }
+
+
+
+
+
+
+
---
+++ UART.h
@@ -0,0 +1,37 @@
+/*
+ * UART.h
+ *
+ * Created: 2/19/2018 12:27:16 AM
+ *  Author: Ahmed
+ */ 
+
+
+#ifndef UART_H_
+#define UART_H_
+
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+#ifndef F_CPU
+#define F_CPU 16000000UL
+#endif
+
+
+#define BAUD_RATE 115200
+#define UBRR_VALUE (F_CPU / ((16 * BAUD_RATE) + 1))
+
+/******************** Functions **************************/
+
+void UART_init();
+void UART_sendByte(uint8_t data);
+uint8_t UART_receiveByte();
+void UART_sendString(const uint8_t *str);
+void UART_receiveString(uint8_t *str);
+
+
+
+
+
+
+#endif /* UART_H_ */
