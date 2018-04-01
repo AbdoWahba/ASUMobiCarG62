177d044f2fe515437439532dd20065096dde625b
vawzy <33329584+vawzy@users.noreply.github.com>
2018-04-01 14:42:22 +0200

    Add files via upload

---
+++ Members' Indvidual Tasks (Learning Phase)/Ahmed Mohamed Fawzy/lcd pushing string.c
@@ -0,0 +1,48 @@
+/*
+ * lcd.c
+ *
+ * Created: 3/2/2018 2:38:56 AM
+ * Author : Dell
+ */ 
+
+#ifndef F_CPU
+#define F_CPU 16000000UL 
+#endif
+#define D4 eS_PORTD4
+#define D5 eS_PORTD5
+#define D6 eS_PORTD6
+#define D7 eS_PORTD7
+#define RS eS_PORTC6
+#define EN eS_PORTC7
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include "lcd.h" 
+
+int main(void)
+{
+	DDRD = 0xFF;
+	DDRC = 0xFF;
+	int i;
+	Lcd4_Init();
+	while(1)
+	{
+		Lcd4_Set_Cursor(1,1);
+		Lcd4_Write_String("hello everyone");
+		for(i=0;i<15;i++)
+		{
+			_delay_ms(500);
+			Lcd4_Shift_Left();
+		}
+		for(i=0;i<15;i++)
+		{
+			_delay_ms(500);
+			Lcd4_Shift_Right();
+		}
+		Lcd4_Clear();
+		Lcd4_Set_Cursor(2,1);
+		Lcd4_Write_Char('e');
+		Lcd4_Write_Char('S');
+		_delay_ms(2000);
+	}
+}
